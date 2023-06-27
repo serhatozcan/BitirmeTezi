866e128d97280218462e75b66da25ef11c1c65c0
serhatozcan <68111189+serhatozcan@users.noreply.github.com>
2023-06-27 06:27:54 +0300

    Update GameOver.cs

--- Bitirme Tezi/Assets/Scenes/GameScripts/GameOver.cs
+++ Bitirme Tezi/Assets/Scenes/GameScripts/GameOver.cs
@@ -8,37 +8,22 @@ using UnityEngine.UI;
 public class GameOver : MonoBehaviour
 {
     //public GameObject gameOverPanel;
-    public int catNumber, levelNumber;
-    public TMP_Text errorMessageText, codeInputField_1Text, codeInputField_2Text;
+    //public int catNumber, levelNumber;
+    public TMP_Text errorMessageText;
+        //, codeInputField_1Text, codeInputField_2Text;
     //public string codeInputField_1Text, codeInputField_2Text;
-    public void SetupForTwoCodeInputs(int CatNumberInput, int levelNumberInput, string errorMessage, string codeInputField_1, string codeInputField_2)
+    public void Setup(string errorMessage)
     {
         gameObject.SetActive(true);
         Time.timeScale = 0;
-        catNumber = CatNumberInput;
-        levelNumber = levelNumberInput;
+        //catNumber = CatNumberInput;
+        //levelNumber = levelNumberInput;
         errorMessageText.text = errorMessage;
-        //codeInputField_1Text.text = codeInputField_1;
-        PlayerPrefs.SetString("codeInput1", codeInputField_1);
-        PlayerPrefs.SetString("codeInput2", codeInputField_2);
-        //codeInputField_2Text.text = codeInputField_2;
+        ////codeInputField_1Text.text = codeInputField_1;
+        //PlayerPrefs.SetString("codeInput1", codeInputField_1);
+        //PlayerPrefs.SetString("codeInput2", codeInputField_2);
+        ////codeInputField_2Text.text = codeInputField_2;
         
-    }
-
-    public void SetupForOneCodeInput(int CatNumberInput, int levelNumberInput, string errorMessage, string codeInputField)
-    {
-
-    }
-
-    public void Test()
-    {
-
-    }
-
-    public void RestartWithCodes()
-    {
-        //gameOverPanel.SetActive(false);
-
     }
 
 }
